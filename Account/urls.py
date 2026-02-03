0e469ad4277abd1b59a37099bb4430d19e7d8b57
krishdesai-code <krishdesai183@gmail.com>
2026-02-03 15:09:54 +0530

    User Register and login system added!

--- Account/urls.py
+++ Account/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("register/",views.registerPage,name="registerPage"),
     path("login/",views.loginPage,name="loginPage"),
+    path("logout/",views.logoutPage,name="logout"),
+    path("home/",views.homePage,name="home")
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import User
+from django.contrib.auth.hashers import make_password
 
 def registerPage(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        hased_pass = make_password(password)
+
+        User.objects.create(
+            name=name,
+            email=email,
+            password=hased_pass,
+        )
+        return redirect("loginPage")
     return render(request,"account/register.html")
 
 def loginPage(request):
+    if request.method == 'POST':
+        email = request.POST.get("email") 
+        password = request.POST.get("password")
+
+        hased_pass = make_password(password)
+        user = User.objects.get(email=email,password=hased_pass)
+        
+        if(user):
+            request.session['email'] = user.email
+            return redirect("home")
+        else :
+            return render(request,"account/login.html",{'error' : 'Invalid email or password'})
+
     return render(request,"account/login.html")
+
+def homePage(request):
+    return render(request,"account/home.html")
+
+def logoutPage(request):
+    request.session.flush()
+    return redirect("/")
