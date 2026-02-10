8d634bb1b8d39494ee83c6a3e16aba40a5bc8db6
krishdesai-code <krishdesai183@gmail.com>
2026-02-10 12:40:33 +0530

    LoginBug Fixed and middleware added!

--- Account/views.py
+++ Account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import User
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import check_password
+
 
 def registerPage(request):
     if request.method == "POST":
@@ -22,21 +24,26 @@ def loginPage(request):
     if request.method == 'POST':
         email = request.POST.get("email") 
         password = request.POST.get("password")
-
-        hased_pass = make_password(password)
-        user = User.objects.get(email=email,password=hased_pass)
-        
-        if(user):
-            request.session['user_id'] = user.id
-            request.session['email'] = user.email
-            request.session['name'] = user.name
-            return redirect("home")
-        else :
-            return render(request,"account/login.html",{'error' : 'Invalid email or password'})
-
-    return render(request,"account/login.html")
+        try:
+            user = User.objects.get(email=email)
+            if check_password(password, user.password):
+                request.session['user_id'] = user.id
+                request.session['email'] = user.email
+                request.session['name'] = user.name
+                return redirect("home")
+            else:
+                return render(request, "account/login.html", {
+                    'error': 'Invalid email or password'
+                })
+        except User.DoesNotExist:
+            return render(request, "account/login.html", {
+                'error': 'Invalid email or password'
+            })
+    return render(request, "account/login.html")
 
 def homePage(request):
+    if not request.session.get('user_id'):
+        return redirect("loginPage")
     return render(request,"account/home.html")
 
 def logoutPage(request):
