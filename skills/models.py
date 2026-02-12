d94a865d8e91a7227add911d7d34143e2fcf3a8b
krishdesai-code <krishdesai183@gmail.com>
2026-02-12 14:05:48 +0530

    SkillCategory Section Added!!

--- skills/models.py
+++ skills/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from Account.models import User
-class Skills(models.Model):
 
+class Skill_category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+class Skills(models.Model):
     Level_option = [
         ('Basic','Basic'),
         ('Intermediate','Intermediate'),
@@ -12,6 +16,7 @@ class Skills(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     skill_title = models.CharField(max_length=30)
     skill_description = models.CharField(max_length=200)
+    skill_category = models.ForeignKey(Skill_category,on_delete=models.CASCADE,related_name='skills')
     skill_level = models.CharField(max_length=20,choices=Level_option)
     skill_links = models.URLField(null=True)
 
